d015378966db3307b680cb892309befdbc262ffb
Ivan Perzhinsky <94743980+xzripper@users.noreply.github.com>
2023-10-14 21:11:52 +0300

    time_manager.
    time_manager.

---
+++ time_manager/__init__.py
@@ -0,0 +1,125 @@
+"""Time Manager for Python."""
+
+from time import sleep, perf_counter, perf_counter_ns
+
+from threading import Thread
+
+from typing import Union, Callable
+
+
+TM_VERSION = 1.0
+
+TimeUnit = int
+
+TimeProcess = Callable[[], None]
+
+NANOSECOND: TimeUnit = 1
+MICROSECOND: TimeUnit = 2
+MILLISECOND: TimeUnit = 3
+SECOND: TimeUnit = 4
+MINUTE: TimeUnit = 5
+HOUR: TimeUnit = 6
+DAY: TimeUnit = 7
+WEEK: TimeUnit = 8
+MONTH: TimeUnit = 9
+YEAR: TimeUnit = 10
+
+time_counter: Callable[[], float] = perf_counter
+time_counter_ns: Callable[[], int] = perf_counter_ns
+
+def time_thread(time_process: TimeProcess, daemon: bool=False) -> None:
+    """Run time process in a thread."""
+    Thread(target=time_process, daemon=daemon).start()
+
+def do_process(time_process: TimeProcess) -> None:
+    """Run time process without thread."""
+    time_process()
+
+def t_round(number: float, round_float: bool=True) -> int:
+    """
+    Round number.
+
+    `round_float` is True:
+    ```
+    t_round(5.75) # => 5
+    t_round(1.0) # => 1
+    ```
+
+    `round_float` is False:
+    ```
+    t_round(5.75) # => 5.75
+    t_round(1.0) # => 1
+
+    t_round(456.4859) # => 456.4859
+    t_round(456.0) # => 456
+    """
+    if round_float:
+        return round(number)
+
+    else:
+        digits = str(number).split('.')
+
+        if len(digits) > 1:
+            if int(digits[1]) == 0:
+                return round(number)
+
+        else:
+            return number
+
+def wait(delay: float) -> None:
+    """Wait (sleep)."""
+    sleep(delay)
+
+def do_and_wait(callable: Callable, delay: float) -> None:
+    """Call callable and wait."""
+    callable()
+
+    wait(delay)
+
+def call_after(callable: Callable, delay: float) -> TimeProcess:
+    """Call function after delay."""
+    return lambda: [wait(delay), callable()]
+
+def call_after2(callable1: Callable, callable2: Callable, delay: float) -> TimeProcess:
+    """Call callable1, call callable2 after delay."""
+    return lambda: [callable1(), wait(delay), callable2()]
+
+def do(callable: Callable, delay: float, times: int) -> TimeProcess:
+    """Call callable X times."""
+    def _process():
+        for _ in range(times):
+            callable()
+
+            wait(delay)
+
+    return _process
+
+def do_for(callable: Callable, do_seconds: float) -> TimeProcess:
+    """Do callable for some time."""
+    def _process():
+        passed = 0
+
+        while passed <= do_seconds:
+            before_callable = perf_counter()
+
+            callable()
+
+            passed += perf_counter() - before_callable
+
+    return _process
+
+def code_exec_time(callable: Callable, ns: bool=False) -> float:
+    """Get code execution time."""
+    if ns:
+        start = perf_counter_ns()
+
+        callable()
+
+        return perf_counter_ns() - start
+
+    else:
+        start = perf_counter()
+
+        callable()
+
+        return perf_counter() - start
---
+++ time_manager/uc.py
@@ -0,0 +1,158 @@
+"""Unit Converter."""
+
+from . import t_round, Union, TimeUnit, NANOSECOND, MICROSECOND, MILLISECOND, SECOND, MINUTE, HOUR, DAY, WEEK, MONTH, YEAR, TM_VERSION
+
+
+def translate_time_ns(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate nanoseconds to other unit."""
+    if unit == NANOSECOND: return t_round(time)
+    elif unit == MICROSECOND: return t_round(time / 1000, False)
+    elif unit == MILLISECOND: return t_round(time / 1e+6, False)
+    elif unit == SECOND: return t_round(time / 1e+9, False)
+    elif unit == MINUTE: return t_round(time / 6e+10, False)
+    elif unit == HOUR: return t_round(time / 3.6e+12, False)
+    elif unit == DAY: return t_round(time / 8.64e+13, False)
+    elif unit == WEEK: return t_round(time / 6.048e+14, False)
+    elif unit == MONTH: return t_round(time / 2.628e+15, False)
+    elif unit == YEAR: return t_round(time / 3.154e+16, False)
+    else: return t_round(time)
+
+def translate_time_mis(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate microseconds to other unit."""
+    if unit == NANOSECOND: return t_round(time * 1000, False)
+    elif unit == MICROSECOND: return t_round(time)
+    elif unit == MILLISECOND: return t_round(time / 1000, False)
+    elif unit == SECOND: return t_round(time / 1e+6, False)
+    elif unit == MINUTE: return t_round(time / 6e+7, False)
+    elif unit == HOUR: return t_round(time / 3.6e+9, False)
+    elif unit == DAY: return t_round(time / 8.64e+10, False)
+    elif unit == WEEK: return t_round(time / 6.048e+11, False)
+    elif unit == MONTH: return t_round(time / 2.628e+12, False)
+    elif unit == YEAR: return t_round(time / 3.154e+13, False)
+    else: return t_round(time)
+
+def translate_time_ms(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate milliseconds to other unit."""
+    if unit == NANOSECOND: return t_round(time * 1e+6, False)
+    elif unit == MICROSECOND: return t_round(time * 1000, False)
+    elif unit == MILLISECOND: return t_round(time)
+    elif unit == SECOND: return t_round(time / 1000, False)
+    elif unit == MINUTE: return t_round(time / 60000, False)
+    elif unit == HOUR: return t_round(time / 3.6e+6, False)
+    elif unit == DAY: return t_round(time / 8.64e+7, False)
+    elif unit == WEEK: return t_round(time / 6.048e+8, False)
+    elif unit == MONTH: return t_round(time / 2.628e+9, False)
+    elif unit == YEAR: return t_round(time / 3.154e+10, False)
+    else: return t_round(time)
+
+def translate_time_s(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate seconds to other unit."""
+    if unit == NANOSECOND: return t_round(time * 1e+9, False)
+    elif unit == MICROSECOND: return t_round(time * 1e+6, False)
+    elif unit == MILLISECOND: return t_round(time * 1000, False)
+    elif unit == SECOND: return t_round(time)
+    elif unit == MINUTE: return t_round(time / 60, False)
+    elif unit == HOUR: return t_round(time / 3600, False)
+    elif unit == DAY: return t_round(time / 86400, False)
+    elif unit == WEEK: return t_round(time / 604800, False)
+    elif unit == MONTH: return t_round(time / 2.628e+6, False)
+    elif unit == YEAR: return t_round(time / 3.154e+7, False)
+    else: return t_round(time)
+
+def translate_time_m(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate minutes to other unit."""
+    if unit == NANOSECOND: return t_round(time * 3.6e+12, False)
+    elif unit == MICROSECOND: return t_round(time * 6e+7, False)
+    elif unit == MILLISECOND: return t_round(time * 60000, False)
+    elif unit == SECOND: return t_round(time * 60, False)
+    elif unit == MINUTE: return t_round(time)
+    elif unit == HOUR: return t_round(time / 60, False)
+    elif unit == DAY: return t_round(time / 1440, False)
+    elif unit == WEEK: return t_round(time / 10080, False)
+    elif unit == MONTH: return t_round(time / 43800, False)
+    elif unit == YEAR: return t_round(time / 525600, False)
+    else: return t_round(time)
+
+def translate_time_h(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate hour to other unit."""
+    if unit == NANOSECOND: return t_round(time * 3.6e+12, False)
+    elif unit == MICROSECOND: return t_round(time * 3.6e+9, False)
+    elif unit == MILLISECOND: return t_round(time * 3.6e+6, False)
+    elif unit == SECOND: return t_round(time * 3600, False)
+    elif unit == MINUTE: return t_round(time * 60, False)
+    elif unit == HOUR: return t_round(time)
+    elif unit == DAY: return t_round(time / 24, False)
+    elif unit == WEEK: return t_round(time / 168, False)
+    elif unit == MONTH: return t_round(time / 730, False)
+    elif unit == YEAR: return t_round(time / 8760, False)
+    else: return t_round(time)
+
+def translate_time_d(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate day to other unit."""
+    if unit == NANOSECOND: return t_round(time * 8.64e+13, False)
+    elif unit == MICROSECOND: return t_round(time * 8.64e+10, False)
+    elif unit == MILLISECOND: return t_round(time * 8.64e+7, False)
+    elif unit == SECOND: return t_round(time * 86400, False)
+    elif unit == MINUTE: return t_round(time * 1440, False)
+    elif unit == HOUR: return t_round(time * 24, False)
+    elif unit == DAY: return t_round(time)
+    elif unit == WEEK: return t_round(time / 7, False)
+    elif unit == MONTH: return t_round(time / 30.417, False)
+    elif unit == YEAR: return t_round(time / 365, False)
+    else: return t_round(time)
+
+def translate_time_w(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate week to other unit."""
+    if unit == NANOSECOND: return t_round(time * 6.048e+14, False)
+    elif unit == MICROSECOND: return t_round(time * 6.048e+11, False)
+    elif unit == MILLISECOND: return t_round(time * 6.048e+8, False)
+    elif unit == SECOND: return t_round(time * 604800, False)
+    elif unit == MINUTE: return t_round(time * 10080, False)
+    elif unit == HOUR: return t_round(time * 168, False)
+    elif unit == DAY: return t_round(time * 7, False)
+    elif unit == WEEK: return t_round(time)
+    elif unit == MONTH: return t_round(time / 4.345, False)
+    elif unit == YEAR: return t_round(time / 52.143, False)
+    else: return t_round(time)
+
+def translate_time_mo(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate month to other unit."""
+    if unit == NANOSECOND: return t_round(time * 2.628e+15, False)
+    elif unit == MICROSECOND: return t_round(time * 2.628e+12, False)
+    elif unit == MILLISECOND: return t_round(time * 2.628e+9, False)
+    elif unit == SECOND: return t_round(time * 2.628e+6, False)
+    elif unit == MINUTE: return t_round(time * 43800, False)
+    elif unit == HOUR: return t_round(time * 730, False)
+    elif unit == DAY: return t_round(time * 30.417, False)
+    elif unit == WEEK: return t_round(time * 4.345, False)
+    elif unit == MONTH: return t_round(time)
+    elif unit == YEAR: return t_round(time / 12, False)
+    else: return t_round(time)
+
+def translate_time_y(time: float, unit: TimeUnit) -> Union[int, float]:
+    """Translate year to other unit."""
+    if unit == NANOSECOND: return t_round(time * 3.154e+16, False)
+    elif unit == MICROSECOND: return t_round(time * 3.154e+13, False)
+    elif unit == MILLISECOND: return t_round(time * 3.54e+10, False)
+    elif unit == SECOND: return t_round(time * 3.154e+7, False)
+    elif unit == MINUTE: return t_round(time * 525600, False)
+    elif unit == HOUR: return t_round(time * 8760, False)
+    elif unit == DAY: return t_round(time * 365, False)
+    elif unit == WEEK: return t_round(time * 52.143, False)
+    elif unit == MONTH: return t_round(time * 12, False)
+    elif unit == YEAR: return t_round(time)
+    else: return t_round(time)
+
+def translate_time(time: float, time_unit: TimeUnit, convert_to: TimeUnit) -> Union[int, float]:
+    """Translate one unit to another."""
+    if time_unit == convert_to: return t_round(time)
+    elif time_unit == NANOSECOND: return translate_time_ns(time, convert_to)
+    elif time_unit == MICROSECOND: return translate_time_mis(time, convert_to)
+    elif time_unit == MILLISECOND: return translate_time_ms(time, convert_to)
+    elif time_unit == SECOND: return translate_time_s(time, convert_to)
+    elif time_unit == MINUTE: return translate_time_m(time, convert_to)
+    elif time_unit == HOUR: return translate_time_h(time, convert_to)
+    elif time_unit == DAY: return translate_time_d(time, convert_to)
+    elif time_unit == WEEK: return translate_time_w(time, convert_to)
+    elif time_unit == YEAR: return translate_time_y(time, convert_to)
+    else: return t_round(time)
